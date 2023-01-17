e92fbf94db21d09ffdebbb4aa5785e76be7c740d
koltikin <koltikin210@gmail.com>
2023-01-17 12:41:31 +0300

    remove mate

--- my_store/models.py
+++ my_store/models.py
@@ -45,10 +45,6 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRPONZE
     )
 
-    class Meta:
-        db_table = "store_customers"
-        indexes = [models.Index(fields=["last_name", "first_name"])]
-
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = "P"
